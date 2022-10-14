37d4344133f0561ee27266e5616cc6eb2c00cf77
ralo <leona8464@gmail.com>
2022-10-14 19:30:18 +0900

    #7.5 Errors

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -14,9 +14,15 @@ interface LoginForm {
 }
 
 export default function Forms() {
-  const { register, handleSubmit } = useForm<LoginForm>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginForm>({ mode: "onBlur" }); //input을 나가면 발생
   const onValid = (data: LoginForm) => {};
-  const onInvalid = (errors: FieldErrors) => {};
+  const onInvalid = (errors: FieldErrors) => {
+    console.log(errors);
+  };
   return (
     <form onSubmit={handleSubmit(onValid, onInvalid)}>
       <input
@@ -31,10 +37,18 @@ export default function Forms() {
         placeholder="Username"
       />
       <input
-        {...register("email", { required: "Email is required" })}
+        {...register("email", {
+          required: "Email is required",
+          validate: {
+            notGmail: (value) =>
+              !value.includes("@gmail.com") || "Gmail is not allowed",
+          },
+        })}
         type="email"
         placeholder="Email"
+        className={`${Boolean(errors.email) ? "border-red-500" : ""}`}
       />
+
       <input
         {...register("password", { required: "Password is required" })}
         type="password"
